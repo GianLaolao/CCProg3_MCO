13de38ceb4000454ab3df0e1e780f7ba7eb2d50c
GianLaolao <gian_laolao@dlsu.edu.ph>
2023-06-17 10:29:07 +0800

    class

---
+++ src/Item.java
@@ -0,0 +1,44 @@
+
+public class Item {
+
+    private String name;
+    private int price;
+    private float calories;
+    private int quantity;
+
+    public Item (String name, int price, float calories) {
+        this.name = name;
+        this.price = price;
+        this.calories = calories;
+    }
+
+     public Item (String name, int price, float calories, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.calories = calories;
+        this.quantity = quantity;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public float getCalories() {
+        return calories;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
--- src/MoneyBox.java
+++ src/MoneyBox.java
@@ -1,3 +1,8 @@
 public class MoneyBox {
     
+    private int onePeso;
+    private int fivePeso;
+    private int tenPeso;
+    private int fiftyPeso;
+    private int hundredPeso;
 }
