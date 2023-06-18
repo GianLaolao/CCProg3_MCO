859623d923724726da374cbe4445ab67afafa612
GianLaolao <gian_laolao@dlsu.edu.ph>
2023-06-18 17:15:54 +0800

    pseudocode

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         
+        RegularVendo vendingMachine = new RegularVendo();
+        Main main = new Main();
+
+        //codes for input output
     }
+
+    public void vendingFeatures (RegularVendo vendingMachine) {
+
+        //code for vending features
+    }
+
+    public void maintenanceFeatures (RegularVendo vendingMachine) {
+
+        //code for maintenance features
+    }
+
 }
--- src/RegularVendo.java
+++ src/RegularVendo.java
@@ -1,3 +1,50 @@
+
+import java.util.*;
+
 public class RegularVendo {
-    
+
+    private ArrayList<Item> item = new ArrayList<>();
+    private MoneyBox money;
+
+    public void addItem(Item item) {
+        this.item.add(item);
+    }
+    public void addMoney(int quanity, int type) {
+        
+        switch(type) {
+            case 1:
+                //code for adding one peso 
+            case 2:
+                //code for adding five peso 
+            case 3: 
+                //code for adding ten peso 
+            case 4:
+                //code for adding fifty peso 
+            case 5: 
+                //code for adding hundred peso 
+        }
+    }
+
+    public ArrayList<Item> getItem() {
+        return item;
+    }
+    public MoneyBox getMoney() {
+        return money;
+    }
+
+    public boolean dispenseItem(int itemNum, int quantity) {
+
+        //code for successful dispense of item
+
+            // return true;
+
+        
+        return false;
+    }
+
+    public int retrieveMoney() {
+
+        return money.collectMoney();
+    }
+
 }
